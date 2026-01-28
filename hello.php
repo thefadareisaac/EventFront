ac27d13bf3e53be9fdd7298a83afd7fabde5a663
Isaac Fadare <isaacfadare7@gmail.com>
2026-01-28 13:23:10 +0100

    Initial Commit

---
+++ hello.php
@@ -0,0 +1,6 @@
+<?php
+//echo "<center><font size= 10>Hello World</font><center>";
+$firstno = 300;
+$secondno = 500;
+$sum = $firstno + $secondno;
+echo "<center><font size = 10>The sum of the two numbers is: $sum</font></center>"; 
