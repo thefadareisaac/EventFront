f37ffb24b147cb720049ec2fe8f5d4206586ccff
Isaac Fadare <isaacfadare7@gmail.com>
2026-03-31 13:48:09 +0100

    update all file path for accuracy
    after learning how to properly link files, I needed to correct the
    file path of every file

--- hello.php
+++
@@ -1,6 +0,0 @@
-<?php
-//echo "<center><font size= 10>Hello World</font><center>";
-$firstno = 300;
-$secondno = 500;
-$sum = $firstno + $secondno;
-echo "<center><font size = 10>The sum of the two numbers is: $sum</font></center>"; 
